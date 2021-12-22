dab299ecddbd28585d73bb814962a1482efac707
minakusic9 <minakusic9@gmail.com>
2021-12-22 23:34:47 +0100

    Kreiran model sala (hall)

---
+++ database/seeders/HallSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Hall;
+class HallSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Hall::factory(3)->create();
+    }
+}
